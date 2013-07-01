e0b890cb5d941518fb609cbbc3a4fa1625943897
Dan van der Ster <daniel.vanderster@cern.ch>
2013-07-01 13:47:52 +0200

    gpgkey from https...

--- manifests/yum/ceph.pp
+++ manifests/yum/ceph.pp
@@ -1,17 +1,12 @@
 class ceph::yum::ceph (
   $release = 'cuttlefish'
 ) {
-  file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-CEPH':
-    source => 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc',
-    replace => false,
-  }
   yumrepo { "ceph":
     descr   => "Ceph $release repository",
     baseurl => "http://ceph.com/rpm-$release/el6/x86_64/",
-    gpgkey   => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CEPH",
+    gpgkey   => "https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc",
     gpgcheck=> 1,
     enabled => 1,
-    require  => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-CEPH'],
     before   => Package['ceph'],
   }
 }
