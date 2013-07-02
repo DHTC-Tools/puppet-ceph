42167c1b83befbc32d0880839d9749e47d24a6a3
Dan van der Ster <daniel.vanderster@cern.ch>
2013-07-02 10:31:58 +0200

    puppet-lint cleanups for yum

--- manifests/yum/ceph.pp
+++ manifests/yum/ceph.pp
@@ -1,12 +1,13 @@
 class ceph::yum::ceph (
   $release = 'cuttlefish'
 ) {
-  yumrepo { "ceph":
-    descr   => "Ceph $release repository",
-    baseurl => "http://ceph.com/rpm-$release/el6/x86_64/",
-    gpgkey   => "https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc",
-    gpgcheck=> 1,
-    enabled => 1,
+  yumrepo { 'ceph':
+    descr    => "Ceph ${release} repository",
+    baseurl  => "http://ceph.com/rpm-${release}/el6/x86_64/",
+    gpgkey   => \
+      'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc',
+    gpgcheck => 1,
+    enabled  => 1,
     priority => 5,
     before   => Package['ceph'],
   }
