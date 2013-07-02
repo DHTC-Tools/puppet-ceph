9c76cb1aee163b6c9f3edf7840ebabe9c55d6add
Dan van der Ster <daniel.vanderster@cern.ch>
2013-07-02 11:54:51 +0200

    remove backslash

--- manifests/yum/ceph.pp
+++ manifests/yum/ceph.pp
@@ -4,7 +4,7 @@ class ceph::yum::ceph (
   yumrepo { 'ceph':
     descr    => "Ceph ${release} repository",
     baseurl  => "http://ceph.com/rpm-${release}/el6/x86_64/",
-    gpgkey   => \
+    gpgkey   =>
       'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc',
     gpgcheck => 1,
     enabled  => 1,
