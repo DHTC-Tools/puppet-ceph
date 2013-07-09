97be129da6d3c1bcda777271999102bafb67fe2a
Dan van der Ster <daniel.vanderster@cern.ch>
2013-07-09 16:28:25 +0200

    remove the test for ceph-osd-crush

--- spec/defines/ceph_osd_device_spec.rb
+++ spec/defines/ceph_osd_device_spec.rb
@@ -113,18 +113,18 @@ class { 'ceph::osd':
         'require' => 'Exec[ceph-osd-mkfs-56]'
       ) }
 
-      it { should contain_exec('ceph-osd-crush-56').with(
-        'command' => 'ceph osd crush set 56 1.00 root=default room=dummy-room host=dummy-host',
-        'path'    => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin',
-        'require' => 'Exec[ceph-osd-register-56]'
-      ) }
+#      it { should contain_exec('ceph-osd-crush-56').with(
+#        'command' => 'ceph osd crush set 56 1.00 root=default room=dummy-room host=dummy-host',
+#        'path'    => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin',
+#        'require' => 'Exec[ceph-osd-register-56]'
+#      ) }
 
       it { should contain_service('ceph-osd.56').with(
         'ensure'    => 'running',
         'start'     => 'service ceph start osd.56',
         'stop'      => 'service ceph stop osd.56',
         'status'    => 'service ceph status osd.56',
-        'require'   => 'Exec[ceph-osd-crush-56]',
+        'require'   => 'Exec[ceph-osd-register-56]',
         'subscribe' => 'Concat[/etc/ceph/ceph.conf]'
       ) }
     end
