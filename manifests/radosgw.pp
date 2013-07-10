dd3d5dbe1bcb1ab7ae6e397fe20c2599f5853d52
Dan van der Ster <daniel.vanderster@cern.ch>
2013-07-10 11:57:51 +0200

    add basic radosgw (no apache yet)

---
+++ manifests/radosgw.pp
@@ -0,0 +1,53 @@
+# Configure a ceph radosgw
+#
+# == Name
+#   This resource's name is the mon's id and must be numeric.
+# == Parameters
+# [*fsid*] The cluster's fsid.
+#   Mandatory. Get one with `uuidgen -r`.
+#
+# [*auth_type*] Auth type.
+#   Optional. undef or 'cephx'. Defaults to 'cephx'.
+#
+# [*mds_data*] Base path for mon data. Data will be put in a mon.$id folder.
+#   Optional. Defaults to '/var/lib/ceph/mds.
+#
+# == Dependencies
+#
+# none
+#
+# == Authors
+#
+#  Dan van der Ster daniel.vanderster@cern.ch
+#
+# == Copyright
+#
+# Copyright 2013 CERN
+#
+
+define ceph::radosgw (
+  $monitor_secret
+) {
+
+  include 'ceph::package'
+  include 'ceph::conf'
+  include 'ceph::params'
+
+  ensure_packages( [ 'ceph-radosgw' ] )
+
+  ceph::conf::radosgw { $name: }
+
+  exec { 'ceph-radosgw-keyring':
+    command =>"ceph auth get-or-create client.radosgw.${::hostname} osd 'allow rwx' mon 'allow r' --name mon. --key=${monitor_secret} -o /etc/ceph/ceph.client.radosgw.${::hostname}.keyring",
+    path    => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin',
+    creates => "/etc/ceph/ceph.client.radosgw.${::hostname}.keyring",
+    before  => Service["ceph-radosgw"],
+    require => Package['ceph'],
+  }
+
+  service { "ceph-radosgw":
+    ensure   => running,
+    provider => $::ceph::params::service_provider,
+    require  => Exec['ceph-radosgw-keyring'],
+  }
+}
